81b48f69c824319458c6b214cdfbee7e5bdb5e63
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-05-30 01:42:36 +1000

    теперь точно пофиксил лучи

--- src/xrEngine/Environment_misc.cpp
+++ src/xrEngine/Environment_misc.cpp
@@ -484,8 +484,6 @@ void CEnvDescriptorMixer::lerp	(CEnvironment* env, CEnvDescriptor& A, CEnvDescri
 	wind_velocity			=	fi*A.wind_velocity + f*B.wind_velocity;
 	wind_direction			=	fi*A.wind_direction + f*B.wind_direction;
 
-	m_fSunShaftsIntensity *= 1.0f - ps_r2_sun_shafts_min;
-	m_fSunShaftsIntensity += ps_r2_sun_shafts_min;
 	m_fSunShaftsIntensity = fi * A.m_fSunShaftsIntensity + f * B.m_fSunShaftsIntensity;
 	m_fWaterIntensity		=	fi*A.m_fWaterIntensity + f*B.m_fWaterIntensity;
 
