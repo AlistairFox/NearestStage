a06ebbadf5a59f23a270d6504733f95c7e5deb9c
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-06-13 05:28:40 +1000

    приложение запуска сервера
    содержит коды запуска ядерных боеголовок

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include "resource.h"
+
+void main()
+{
+	int incode;
+
+	std::cout << "input password";
+	std::cin >> incode;
+	
+	if (incode == 123)
+	{
+		system("start ..\\_dedicated_\\xrEngine.exe -fucknikitakalinovsky1232152123wdf32 -i -fsltx ..\\fsgame_dedicated.ltx -start server(nearestmap/fmp/timelimit=60) client(localhost)");
+	}
+	else
+	{
+		std::cout << "poshel nahui";
+		std::cin >> incode;
+	}
+}
+
