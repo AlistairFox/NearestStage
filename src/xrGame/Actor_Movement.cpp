d265dd90f2f2fafe59a6621644a0e780284212b2
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-06-15 01:15:07 +1000

    откат правок по инерции ходьбы

--- src/xrGame/Actor_Movement.cpp
+++ src/xrGame/Actor_Movement.cpp
@@ -464,20 +464,15 @@ void CActor::g_cl_Orientate	(u32 mstate_rl, float dt)
 		return;
 	}
 
-	if (eacLookAt == cam_active)
+	if (eacFreeLook != cam_active)
 	{
 		r_torso.yaw = cam_Active()->GetWorldYaw();
 		r_torso.pitch = cam_Active()->GetWorldPitch();
 	}
-	else if (eacFreeLook == cam_active)
-	{
-		r_torso.yaw = cam_FirstEye()->GetWorldYaw();
-		r_torso.pitch = cam_FirstEye()->GetWorldPitch();
-	}
 	else
 	{
 		r_torso.yaw = cam_FirstEye()->GetWorldYaw();
-		r_torso.pitch = 0;
+		r_torso.pitch = cam_FirstEye()->GetWorldPitch();
 	}
 
 	if (MpSafeMODE())
@@ -503,40 +498,18 @@ void CActor::g_cl_Orientate	(u32 mstate_rl, float dt)
 		r_model_yaw		= angle_normalize(r_torso.yaw);
 		mstate_real		&=~mcTurn;
 	} else {
-		if (eacFirstEye != cam_active)
-		{
-			// if camera rotated more than 45 degrees - align model with it
-			float ty = angle_normalize(r_torso.yaw);
-			if (_abs(r_model_yaw - ty) > PI_DIV_4) {
-				r_model_yaw_dest = ty;
-				// 
-				mstate_real |= mcTurn;
-			}
-			if (_abs(r_model_yaw - r_model_yaw_dest) < EPS_L) {
-				mstate_real &= ~mcTurn;
-			}
-			if (mstate_rl & mcTurn) {
-				angle_lerp(r_model_yaw, r_model_yaw_dest, PI_MUL_2, dt);
-			}
+		// if camera rotated more than 45 degrees - align model with it
+		float ty = angle_normalize(r_torso.yaw);
+		if (_abs(r_model_yaw - ty) > PI_DIV_4) {
+			r_model_yaw_dest = ty;
+			// 
+			mstate_real |= mcTurn;
 		}
-		else {
-			// if camera rotated more than 35 degrees - align model with it
-			float ty = angle_normalize(r_torso.yaw);
-			if (_abs(r_model_yaw - ty) > PI_DIV_6) {
-				if ((r_model_yaw - ty) > 0)
-					r_model_yaw = angle_normalize(r_torso.yaw + PI_DIV_6);
-				else
-					r_model_yaw = angle_normalize(r_torso.yaw - PI_DIV_6);
-				r_model_yaw_dest = ty;
-				// 
-				mstate_real |= mcTurn;
-			}
-			if (_abs(r_model_yaw - r_model_yaw_dest) < EPS_L) {
-				mstate_real &= ~mcTurn;
-			}
-			if (mstate_rl & mcTurn) {
-				angle_lerp(r_model_yaw, r_model_yaw_dest, PI_MUL_2, dt);
-			}
+		if (_abs(r_model_yaw - r_model_yaw_dest) < EPS_L) {
+			mstate_real &= ~mcTurn;
+		}
+		if (mstate_rl & mcTurn) {
+			angle_lerp(r_model_yaw, r_model_yaw_dest, PI_MUL_2, dt);
 		}
 	}
 }
