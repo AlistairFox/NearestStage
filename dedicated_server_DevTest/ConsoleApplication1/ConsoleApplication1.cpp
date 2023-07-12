83d79c5ada8bb3f87eff365877fed40e047a4ae0
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-07-12 20:31:37 +1000

    либы x64

--- dedicated_server_DevTest/ConsoleApplication1/ConsoleApplication1.cpp
+++ dedicated_server_DevTest/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,22 +1,28 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include "resource.h"
+
 
 void main()
 {
-	int incode;
-
-	std::cout << "input password";
-	std::cin >> incode;
-	
-	if (incode == 12345678)
-	{
-		system("start ..\\_dedicated_\\xrEngine.exe -fucknikitakalinovsky1232152123wdf32 -i -fsltx ..\\fsgame_dedicated.ltx -start server(nearest_test/fmp/timelimit=60) client(localhost)");
-	}
-	else
-	{
-		std::cout << "poshel nahui";
-		std::cin >> incode;
-	}
 }
 
+class A
+{
+protected:
+	void starter()
+	{
+		int incode;
+		std::cout << "input password";
+		std::cin >> incode;
+
+		if (incode == 12345678)
+		{
+			system("start ..\\_dedicated_\\xrEngine.exe -fucknikitakalinovsky5785412386westdfa23 -i -fsltx ..\\fsgame_dedicated.ltx -start server(nearest_test/fmp/timelimit=60) client(localhost)");
+		}
+		else
+		{
+			std::cout << "poshel nahui";
+			std::cin >> incode;
+		}
+	}
+};
