72420b3ef708c78f8f9f6bcb5084cd0517c683f5
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-09-02 23:53:19 +1000

    приложение на запуск сервера

--- dedicated_server_DevTest/ConsoleApplication1/ConsoleApplication1.cpp
+++ dedicated_server_DevTest/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,28 +1,60 @@
 ﻿#include <iostream>
 #include "resource.h"
-
+#include "header.h"
+#include <string>
 
 void main()
 {
+	main_class().arg1();
 }
 
-class A
+void main_class::starter()
 {
-protected:
-	void starter()
+	std::cout << "input password";
+	std::cin >> incode;
+	if (incode == 1)
 	{
-		int incode;
-		std::cout << "input password";
-		std::cin >> incode;
+		starter_c = start + start2 + start5 + start6;
 
-		if (incode == 12345678)
-		{
-			system("start ..\\_dedicated_\\xrEngine.exe -fucknikitakalinovsky5785412386westdfa23 -i -fsltx ..\\fsgame_dedicated.ltx -start server(nearest_test/fmp/timelimit=60) client(localhost)");
-		}
-		else
-		{
-			std::cout << "poshel nahui";
-			std::cin >> incode;
-		}
+		system(starter_c.c_str());
 	}
-};
+	else
+	{
+		std::cout << "poshel nahui";
+		std::cin >> incode;
+	}
+	std::cout << starter_c;
+}
+
+void main_class::arg1()
+{
+		start = "start ..\\_dedicated_\\xrEngine.exe -save_old_logs -skip_cdb_cache_crc32_check ";
+		arg2();
+}
+
+void main_class::arg2()
+{
+		arg3();
+}
+
+void main_class::arg3()
+{
+		start5 = "-fsltx ..\\fsgame_dedicated.ltx ";
+		arg4();
+}
+
+void main_class::arg4()
+{
+	
+	std::cout << "Enter the level:( 1 nearestmap, 2 nearest_test)";
+	std::cin >> start3;
+	if (start3 == 1)
+	{
+		start4 = " server(nearestmap";
+	}
+	else if (start3 == 2)
+		start4 = " server(nearest_test";
+	in_start_6 = "-start" + start4 + dr;
+	start6 = in_start_6;
+	starter();
+}
---
+++ dedicated_server_DevTest/ConsoleApplication1/header.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#pragma warning(disable : 4996)
+#include <string>
+using namespace std;
+void main();
+
+class main_class
+{
+public:
+	void arg1();
+private:
+	void arg4();
+	void starter();
+	void arg2();
+	void arg3();
+	int incode;
+	string starter_c;
+	string start;
+	string start2 = "-fucknikitakalinovsky5785412386westdfa23 -i ";
+	string start4;
+	string start5;
+	string start6;
+	string in_start_6;
+	string dr = "/fmp/timelimit=120) client(localhost)";
+	int start3;
+};
