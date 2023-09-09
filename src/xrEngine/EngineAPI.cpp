a5235e43357880713483bcba37fdcb692fce13de
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-09-10 05:14:09 +1000

    раздельная инициализация рендеров

--- src/xrEngine/EngineAPI.cpp
+++ src/xrEngine/EngineAPI.cpp
@@ -1,4 +1,4 @@
-// EngineAPI.cpp: implementation of the CEngineAPI class.
+п»ї// EngineAPI.cpp: implementation of the CEngineAPI class.
 //
 //////////////////////////////////////////////////////////////////////
 
@@ -14,47 +14,39 @@ extern xr_token* vid_quality_token;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-void __cdecl dummy(void) {
+void __cdecl dummy		(void)	{
 };
-CEngineAPI::CEngineAPI()
+CEngineAPI::CEngineAPI	()
 {
-	hGame = 0;
-	hRender = 0;
-	hTuner = 0;
-	pCreate = 0;
-	pDestroy = 0;
-	tune_pause = dummy;
-	tune_resume = dummy;
+	hGame			= 0;
+	hRender			= 0;
+	hTuner			= 0;
+	pCreate			= 0;
+	pDestroy		= 0;
+	tune_pause		= dummy	;
+	tune_resume		= dummy	;
 }
 
 CEngineAPI::~CEngineAPI()
 {
 	// destroy quality token here
-	if (vid_quality_token)
-	{
-		for (int i = 0; vid_quality_token[i].name; i++)
-		{
-			xr_free(vid_quality_token[i].name);
-		}
-		xr_free(vid_quality_token);
-		vid_quality_token = NULL;
-	}
+	xr_free(vid_quality_token);
 }
 
 extern u32 renderer_value; //con cmd
-ENGINE_API int g_current_renderer = 0;
+ENGINE_API int g_current_renderer = 0; // R1 or R2, used in xrGame
 
-ENGINE_API bool is_enough_address_space_available()
+ENGINE_API bool is_enough_address_space_available	()
 {
 	SYSTEM_INFO		system_info;
 
 	SECUROM_MARKER_HIGH_SECURITY_ON(12)
 
-		GetSystemInfo(&system_info);
+	GetSystemInfo	( &system_info );
 
 	SECUROM_MARKER_HIGH_SECURITY_OFF(12)
 
-		return			(*(u32*)&system_info.lpMaximumApplicationAddress) > 0x90000000;
+	return			(*(u32*)&system_info.lpMaximumApplicationAddress) > 0x90000000;	
 }
 
 LPCSTR r1_name = "xrRender_R1.dll";
@@ -66,50 +58,45 @@ void CEngineAPI::InitializeNotDedicated()
 {
 	SECUROM_MARKER_HIGH_SECURITY_ON(2)
 
-		// initialize R4
-		if (psDeviceFlags.test(rsR4))
-		{
-			Log("Loading DLL: ", r4_name);
-			hRender = LoadLibrary(r4_name);
-			if (0 == hRender)
-			{
-				// try to load R1
-				Msg("! ...Failed - incompatible hardware/pre-Vista OS.");
-				renderer_value = 3;
-				psDeviceFlags.set(rsR2, TRUE);
-				psDeviceFlags.set(rsR4, FALSE);
-			}
-		}
-
-	if (0 == hRender)
+	// try to initialize R4
+	if (psDeviceFlags.test(rsR4))
 	{
-		// try to initialize R2
-		psDeviceFlags.set(rsR2, TRUE);
-		psDeviceFlags.set(rsR4, FALSE);
-		//psDeviceFlags.set	(rsR3,FALSE);
-		Log("Loading DLL: ", r2_name);
-		hRender = LoadLibrary(r2_name);
-		if (0 == hRender)
+		Log				("Loading DLL:",	r4_name);
+		hRender			= LoadLibrary		(r4_name);
+		g_current_renderer = 2;
+
+		// R4 not supported
+		if (NULL == hRender)
 		{
-			Log("! ...Failed - incompatible hardware. Need to support DX9C!");
-			R_CHK(GetLastError());
-			R_ASSERT(hRender, "! ...Failed - incompatible hardware. Need to support DX9C!");
+			psDeviceFlags.set(rsR4, FALSE);
+			psDeviceFlags.set(rsR2, TRUE);
+			renderer_value = 3;
 		}
 	}
 
-	g_current_renderer = 2;
+	// try to initialize R2
+	if (psDeviceFlags.test(rsR2) || NULL == hRender)
+	{
+		Log("Loading DLL:", r2_name);
+		hRender = LoadLibrary(r2_name);
+		g_current_renderer = 2;
 
-	if (renderer_value == 1)
-		Msg("Current Renderer: %s", vid_quality_token[0].name);
-	else if (renderer_value == 3)
-		Msg("Current Renderer: %s", vid_quality_token[1].name);
-	else if (renderer_value == 5)
-		Msg("Current Renderer: %s", vid_quality_token[2].name);
-	else
-		Msg("Ты че долбаеб? ты какой рендер запустил?");
+		// R2 not supported
+		if (NULL == hRender)
+		{
+			psDeviceFlags.set(rsR2, FALSE);
+			renderer_value = 0;
+		}
+	}
 
-
-	Log("Render_value: ", renderer_value);
+	// try to initialize R1
+	if (NULL == hRender)
+	{
+		Log("Loading DLL:", r1_name);
+		hRender = LoadLibrary(r1_name);
+		g_current_renderer = 1;
+		renderer_value = 0;
+	}
 
 	SECUROM_MARKER_HIGH_SECURITY_OFF(2)
 }
@@ -120,71 +107,88 @@ void CEngineAPI::Initialize(void)
 	//////////////////////////////////////////////////////////////////////////
 	// render
 
-#ifndef DEDICATED_SERVER
-	InitializeNotDedicated();
-#else
-	if (0 == hRender)
-	{
-		Msg("Ты че долбаеб?? выруби сервер!");
+	#ifdef DEDICATED_SERVER
 		// try to load R1
 		psDeviceFlags.set(rsR4, FALSE);
 		//psDeviceFlags.set	(rsR3,FALSE);
 		psDeviceFlags.set(rsR2, FALSE);
+		renderer_value = 0; //con cmd
 
 		Log("Loading DLL:", r1_name);
 		hRender = LoadLibrary(r1_name);
-
-		if (0 == hRender)
-			R_CHK(GetLastError());
+		if (0 == hRender)	R_CHK(GetLastError());
 		R_ASSERT(hRender);
-
 		g_current_renderer = 1;
-	}
-#endif // DEDICATED_SERVER
+	#else
+		InitializeNotDedicated();
+		// try to initialize upper renderers
+		if (NULL == hRender)
+		{
+			renderer_value = 5; //con cmd
+			psDeviceFlags.set(rsR4, TRUE);
+			psDeviceFlags.set(rsR2, FALSE);
+			InitializeNotDedicated();
+		}
+		// all renderers not supported
+		if (NULL == hRender)
+		{
+			R_CHK(GetLastError());
+			R_ASSERT(hRender);
+		}
+	#endif // DEDICATED_SERVER
 
 	Device.ConnectToRender();
 
 	// game	
 	{
-		LPCSTR			g_name = "xrGame.dll";
-		Log("Loading DLL: ", g_name);
-		hGame = LoadLibrary(g_name);
-		if (0 == hGame)	R_CHK(GetLastError());
-		R_ASSERT2(hGame, "Game DLL raised exception during loading or there is no game DLL at all");
-		pCreate = (Factory_Create*)GetProcAddress(hGame, "xrFactory_Create");	R_ASSERT(pCreate);
-		pDestroy = (Factory_Destroy*)GetProcAddress(hGame, "xrFactory_Destroy");	R_ASSERT(pDestroy);
+		LPCSTR			g_name	= "xrGame.dll";
+		Log				("Loading DLL:",g_name);
+		hGame			= LoadLibrary	(g_name);
+		if (0==hGame)	R_CHK			(GetLastError());
+		R_ASSERT2		(hGame,"Game DLL raised exception during loading or there is no game DLL at all");
+		pCreate			= (Factory_Create*)		GetProcAddress(hGame,"xrFactory_Create"		);	R_ASSERT(pCreate);
+		pDestroy		= (Factory_Destroy*)	GetProcAddress(hGame,"xrFactory_Destroy"	);	R_ASSERT(pDestroy);
 	}
 
 	//////////////////////////////////////////////////////////////////////////
 	// vTune
-	tune_enabled = FALSE;
-	if (strstr(Core.Params, "-tune")) {
-		LPCSTR			g_name = "vTuneAPI.dll";
-		Log("Loading DLL:", g_name);
-		hTuner = LoadLibrary(g_name);
-		if (0 == hTuner)	R_CHK(GetLastError());
-		R_ASSERT2(hTuner, "Intel vTune is not installed");
-		tune_enabled = TRUE;
-		tune_pause = (VTPause*)GetProcAddress(hTuner, "VTPause");	R_ASSERT(tune_pause);
-		tune_resume = (VTResume*)GetProcAddress(hTuner, "VTResume");	R_ASSERT(tune_resume);
+	tune_enabled		= FALSE;
+	if (strstr(Core.Params,"-tune"))	{
+		LPCSTR			g_name	= "vTuneAPI.dll";
+		Log				("Loading DLL:",g_name);
+		hTuner			= LoadLibrary	(g_name);
+		if (0==hTuner)	R_CHK			(GetLastError());
+		R_ASSERT2		(hTuner,"Intel vTune is not installed");
+		tune_enabled	= TRUE;
+		tune_pause		= (VTPause*)	GetProcAddress(hTuner,"VTPause"		);	R_ASSERT(tune_pause);
+		tune_resume		= (VTResume*)	GetProcAddress(hTuner,"VTResume"	);	R_ASSERT(tune_resume);
 	}
 }
 
-void CEngineAPI::Destroy(void)
+void CEngineAPI::Destroy	(void)
 {
-	if (hGame) { FreeLibrary(hGame);	hGame = 0; }
-	if (hRender) { FreeLibrary(hRender); hRender = 0; }
-	pCreate = 0;
-	pDestroy = 0;
-	Engine.Event._destroy();
-	XRC.r_clear_compact();
+	if (hGame)				{ FreeLibrary(hGame);	hGame	= 0; }
+	if (hRender)			{ FreeLibrary(hRender); hRender = 0; }
+	pCreate					= 0;
+	pDestroy				= 0;
+	Engine.Event._destroy	();
+	XRC.r_clear_compact		();
 }
 
 extern "C" {
-	typedef bool __cdecl SupportsAdvancedRendering(void);
+	typedef bool __cdecl SupportsAdvancedRendering	(void);
 	//typedef bool _declspec(dllexport) SupportsDX10Rendering();
 	typedef bool _declspec(dllexport) SupportsDX11Rendering();
 };
+
+void add_renderer_mode(LPCSTR name, int id)
+{
+	static int i = 0;
+	vid_quality_token[i].id = id;
+	vid_quality_token[i].name = name;
+	i++;
+	return;
+}
 
 void CEngineAPI::CreateRendererList()
 {
@@ -197,9 +201,30 @@ void CEngineAPI::CreateRendererList()
 	vid_quality_token[1].id = -1;
 	vid_quality_token[1].name = NULL;
 #else
-	// проверяем поддержку R4
-	bool bSupports_r4 = false;
-	Log("Try loading DLL: ", r4_name);
+	BOOL bSupports_r1 = FALSE;
+	Log("Try loading DLL:", r1_name);
+	hRender = LoadLibrary(r1_name);
+	if (hRender)
+	{
+		FreeLibrary(hRender);
+		bSupports_r1 = TRUE;
+	}
+	Log("* supports: ", bSupports_r1);
+	
+	BOOL bSupports_r2 = FALSE;
+	Log("Try loading DLL:", r2_name);
+	hRender = LoadLibrary(r2_name);
+	if (hRender)
+	{
+		SupportsAdvancedRendering* test_rendering = (SupportsAdvancedRendering*)GetProcAddress(hRender, "SupportsAdvancedRendering");
+		R_ASSERT(test_rendering);
+		bSupports_r2 = test_rendering();
+		FreeLibrary(hRender);
+	}
+	Log("* supports: ", bSupports_r2);
+
+	BOOL bSupports_r4 = FALSE;
+	Log("Try loading DLL:", r4_name);
 	SetErrorMode(SEM_FAILCRITICALERRORS);
 	hRender = LoadLibrary(r4_name);
 	SetErrorMode(0);
@@ -214,31 +239,47 @@ void CEngineAPI::CreateRendererList()
 
 	hRender = 0;
 
-	// создаем список 
-	u32 size = 3; // r2a, r2.5
+	u32 size = 1;
+
+	if (bSupports_r1) // r1
+		size += 1; 
+	
+	if (bSupports_r2) // r2a, r2, r2.5
+		size += 3; 
 
 	if (bSupports_r4) // r4
-		size += 1;
+		size += 1; 
+
+	if(!bSupports_r1 && !bSupports_r2 && !bSupports_r4)
+		CHECK_OR_EXIT(!FAILED(E_FAIL), 
+			make_string("РўС‹ Р·Р°С‡РµРј СЂРµРЅРґРµСЂ СѓРєСЂР°Р»?\n%s:%s\n%s:%s\n%s:%s\n%s:%s", r1_name, "СѓРєСЂР°РґРµРЅРѕ", r2_name, "СЃРїРёР·Р¶РµРЅРЅРѕ", r4_name, "РїРѕС‚РµСЂСЏРЅРѕ", "xrRender_R5.dll", "coming soon!"));
 
 	vid_quality_token = xr_alloc<xr_token>(size);
 
-	vid_quality_token[0].id = 1;
-	vid_quality_token[0].name = xr_strdup("renderer_r2a");
-
-	vid_quality_token[1].id = 3;
-	vid_quality_token[1].name = xr_strdup("renderer_r2.5");
+	if (bSupports_r1)
+	{
+		//add_renderer_mode("renderer_r1", 0);
+	}
+	
+	if (bSupports_r2)
+	{
+		add_renderer_mode("renderer_r2a", 1);
+		//add_renderer_mode("renderer_r2", 2);
+		add_renderer_mode("renderer_r2.5", 3);
+	}
 
 	if (bSupports_r4)
 	{
-		vid_quality_token[2].id = 5;
-		vid_quality_token[2].name = xr_strdup("renderer_r4");
+		add_renderer_mode("renderer_r4", 5);
 	}
 
 	vid_quality_token[size - 1].id = -1;
 	vid_quality_token[size - 1].name = NULL;
 
 	Msg("Available render modes[%d]:", size - 1);
-	for (u32 i = 0; i < size -1; ++i)
+	for (u32 i = 0; i < size - 1; ++i)
+	{
 		Msg("[%s]", vid_quality_token[i].name);
+	}
 #endif
 }
