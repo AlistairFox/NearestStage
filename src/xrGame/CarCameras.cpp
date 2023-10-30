8c9a51da122884cbf765d16b2a64a9ffdeb3adb0
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-10-30 23:45:56 +1000

    Revert "гитхаб ебанина"
    This reverts commit 82410abd9507fd85bc4cd2b6e1dbf1f3bab67c2d.

--- src/xrGame/CarCameras.cpp
+++ src/xrGame/CarCameras.cpp
@@ -1,13 +1,10 @@
 #include "stdafx.h"
 #pragma hdrstop
-
 #ifdef DEBUG
-#include "ode_include.h"
-#include "StatGraph.h"
-#include "PHDebug.h"
-#include "phworld.h"
-#endif
 
+#include "PHDebug.h"
+#include "../xrphysics/iphworld.h"
+#endif
 #include "alife_space.h"
 #include "hit.h"
 #include "PHDestroyable.h"
@@ -16,63 +13,58 @@
 #include "cameralook.h"
 #include "camerafirsteye.h"
 #include "level.h"
-#include "cameramanager.h"
+#include "../xrEngine/cameramanager.h"
 
-bool CCar::HUDView() const
+bool CCar::HUDView() const		
 {
-	return active_camera->tag == ectFirst;
+	return active_camera->tag==ectFirst;
 }
 
-void CCar::cam_Update(float dt, float fov)
+void	CCar::cam_Update			(float dt, float fov)
 {
-	DEBUG_VERIFY(!ph_world->Processing());
-	Fvector P, Da;
-	Da.set(0, 0, 0);
+	VERIFY(!physics_world()->Processing());
+	Fvector							P,Da;
+	Da.set							(0,0,0);
 	//bool							owner = !!Owner();
 
-	XFORM().transform_tiny(P, m_camera_position);
+	XFORM().transform_tiny			(P,m_camera_position);
 
-	switch (active_camera->tag)
-	{
+	switch(active_camera->tag) {
 	case ectFirst:
 		// rotate head
-		if (OwnerActor())
-			OwnerActor()->Orientation().yaw = -active_camera->yaw;
-		if (OwnerActor())
-			OwnerActor()->Orientation().pitch = -active_camera->pitch;
+		if(OwnerActor()) OwnerActor()->Orientation().yaw	= -active_camera->yaw;
+		if(OwnerActor()) OwnerActor()->Orientation().pitch	= -active_camera->pitch;
 		break;
-	case ectChase:
-		break;
-	case ectFree:
-		break;
+	case ectChase:						break;
+	case ectFree:						break;
 	}
-	active_camera->f_fov = fov;
-	active_camera->Update(P, Da);
-	Level().Cameras().Update(active_camera);
+	active_camera->f_fov				= fov;
+	active_camera->Update				(P,Da);
+	Level().Cameras().UpdateFromCamera	(active_camera);
 }
 
-void CCar::OnCameraChange(int type)
+void	CCar::OnCameraChange		(int type)
 {
-	if (Owner())
+	if(Owner())
 	{
-		if (type == ectFirst)
+		if	(type==ectFirst)
 		{
 			Owner()->setVisible(FALSE);
 		}
-		else if (active_camera->tag == ectFirst)
+		else if(active_camera->tag==ectFirst)
 		{
 			Owner()->setVisible(TRUE);
 		}
 	}
-
-	if (!active_camera || active_camera->tag != type)
-	{
-		active_camera = camera[type];
-		if (ectFree == type)
-		{
+	
+	if (!active_camera||active_camera->tag!=type){
+		active_camera	= camera[type];
+		if (ectFree==type){
 			Fvector xyz;
 			XFORM().getXYZi(xyz);
-			active_camera->yaw = xyz.y;
+			active_camera->yaw		= xyz.y;
 		}
 	}
+
 }
+
