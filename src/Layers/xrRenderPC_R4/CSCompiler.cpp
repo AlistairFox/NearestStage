1c51335f8ef01752c2b3df4f125146ec70c245b2
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-11-05 01:40:23 +1000

    Revert "Revert "забыл""
    This reverts commit 038f4dd5be06d588e7d9b3715c2cd97c42240e6a.

--- src/Layers/xrRenderPC_R4/CSCompiler.cpp
+++ src/Layers/xrRenderPC_R4/CSCompiler.cpp
@@ -25,53 +25,39 @@ CSCompiler& CSCompiler::defSampler(LPCSTR ResourceName)
 	D3D11_SAMPLER_DESC	desc;
 	ZeroMemory(&desc, sizeof(desc));
 
-	//	Use D3DTADDRESS_CLAMP,	D3DTEXF_POINT,			D3DTEXF_NONE,	D3DTEXF_POINT 
+
 	if (0==xr_strcmp(ResourceName,"smp_nofilter"))
 	{
-		//i_dx10Address( stage, D3DTADDRESS_CLAMP);
-		//i_dx10Filter(stage, D3DTEXF_POINT, D3DTEXF_NONE, D3DTEXF_POINT);
+
 		desc.AddressU = desc.AddressV = desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 		desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 		return defSampler(ResourceName, desc);
 	}
 
-	//	Use D3DTADDRESS_CLAMP,	D3DTEXF_LINEAR,			D3DTEXF_NONE,	D3DTEXF_LINEAR 
 	if (0==xr_strcmp(ResourceName,"smp_rtlinear"))
 	{
-		//i_dx10Address( stage, D3DTADDRESS_CLAMP);
-		//i_dx10Filter(stage, D3DTEXF_LINEAR, D3DTEXF_NONE, D3DTEXF_LINEAR);
 		desc.AddressU = desc.AddressV = desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 		desc.Filter = D3D11_FILTER_MIN_MAG_LINEAR_MIP_POINT;
 		return defSampler(ResourceName, desc);
 	}
 
-	//	Use	D3DTADDRESS_WRAP,	D3DTEXF_LINEAR,			D3DTEXF_LINEAR,	D3DTEXF_LINEAR
 	if (0==xr_strcmp(ResourceName,"smp_linear"))
 	{
-		//i_dx10Address( stage, D3DTADDRESS_WRAP);
-		//i_dx10Filter(stage, D3DTEXF_LINEAR, D3DTEXF_LINEAR, D3DTEXF_LINEAR);
 		desc.AddressU = desc.AddressV = desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 		desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
 		return defSampler(ResourceName, desc);
 	}
 
-	//	Use D3DTADDRESS_WRAP,	D3DTEXF_ANISOTROPIC, 	D3DTEXF_LINEAR,	D3DTEXF_ANISOTROPIC
 	if (0==xr_strcmp(ResourceName,"smp_base"))
 	{
-		//i_dx10Address( stage, D3DTADDRESS_WRAP);
-		//i_dx10FilterAnizo( stage, TRUE);
 		desc.AddressU = desc.AddressV = desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 		desc.Filter = D3D11_FILTER_ANISOTROPIC;
 		desc.MaxAnisotropy = 8;
 		return defSampler(ResourceName, desc);
 	}
 
-	//	Use D3DTADDRESS_CLAMP,	D3DTEXF_LINEAR,			D3DTEXF_NONE,	D3DTEXF_LINEAR
 	if (0==xr_strcmp(ResourceName,"smp_material"))
 	{
-		//i_dx10Address( stage, D3DTADDRESS_CLAMP);
-		//i_dx10Filter(stage, D3DTEXF_LINEAR, D3DTEXF_NONE, D3DTEXF_LINEAR);
-		//RS.SetSAMP(stage,D3DSAMP_ADDRESSW,	D3DTADDRESS_WRAP);
 		desc.AddressU = desc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
 		desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 		desc.Filter = D3D11_FILTER_MIN_MAG_LINEAR_MIP_POINT;
@@ -80,10 +66,6 @@ CSCompiler& CSCompiler::defSampler(LPCSTR ResourceName)
 
 	if (0==xr_strcmp(ResourceName,"smp_smap"))
 	{
-		//i_dx10Address( stage, D3DTADDRESS_CLAMP);
-		//i_dx10Filter(stage, D3DTEXF_LINEAR, D3DTEXF_NONE, D3DTEXF_LINEAR);
-		//RS.SetSAMP(stage, XRDX10SAMP_COMPARISONFILTER, TRUE);
-		//RS.SetSAMP(stage, XRDX10SAMP_COMPARISONFUNC, D3D_COMPARISON_LESS_EQUAL);
 		desc.AddressU = desc.AddressV = desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 		desc.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_LINEAR_MIP_POINT;
 		desc.ComparisonFunc = D3D_COMPARISON_LESS_EQUAL;
@@ -92,8 +74,6 @@ CSCompiler& CSCompiler::defSampler(LPCSTR ResourceName)
 
 	if (0==xr_strcmp(ResourceName,"smp_jitter"))
 	{
-		//i_dx10Address( stage, D3DTADDRESS_WRAP);
-		//i_dx10Filter(stage, D3DTEXF_POINT, D3DTEXF_NONE, D3DTEXF_POINT);
 		desc.AddressU = desc.AddressV = desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 		desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 		return defSampler(ResourceName, desc);
