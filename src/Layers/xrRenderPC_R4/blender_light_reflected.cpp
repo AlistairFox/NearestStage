88491d5779195c0f4c2847ff1e48cc5d0e295f1a
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2023-11-05 01:33:26 +1000

    Update blender_light_reflected.cpp

--- src/Layers/xrRenderPC_R4/blender_light_reflected.cpp
+++ src/Layers/xrRenderPC_R4/blender_light_reflected.cpp
@@ -9,9 +9,10 @@ CBlender_accum_reflected::~CBlender_accum_reflected	()	{	}
 void CBlender_accum_reflected::Compile(CBlender_Compile& C)
 {
 	IBlender::Compile(C);
+	BOOL		blend = RImplementation.o.fp16_blend;
 	D3D11_BLEND	dest = D3D11_BLEND_ONE;
 
-	C.r_Pass("accum_volume", "accum_indirect_nomsaa", false, false, false, true, D3D11_BLEND_ONE, dest);
+	C.r_Pass("accum_volume", "accum_indirect_nomsaa", false, false, false, blend, D3D11_BLEND_ONE, dest);
 	C.r_dx10Texture("s_position", r2_RT_P);
 	C.r_dx10Texture("s_normal", r2_RT_N);
 	C.r_dx10Texture("s_material", r2_material);
