79cf6629a8735cac4634bb8132da1d966ab54fef
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2024-01-29 21:25:26 +1000

    Update HUDManager.cpp

--- src/xrGame/HUDManager.cpp
+++ src/xrGame/HUDManager.cpp
@@ -168,8 +168,8 @@ void CHUDManager::Render_First()
 	if (0==O)						return;
 	CActor*		A					= smart_cast<CActor*> (O);
 
-	if (!A)							return;
-	if (A && !A->HUDview())			return;
+	if (!A || !A->HUDview())
+		return;
 
 	// only shadow 
 	::Render->set_Invisible			(TRUE);
