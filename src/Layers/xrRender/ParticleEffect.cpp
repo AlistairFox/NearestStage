0147b748ed8123b8de1358e637721f93ce907131
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2024-01-29 22:50:35 +1000

    фикс худовых партиклов

--- src/Layers/xrRender/ParticleEffect.cpp
+++ src/Layers/xrRender/ParticleEffect.cpp
@@ -515,9 +515,9 @@ void CParticleEffect::Render(float)
 				Fmatrix FTold = Device.mFullTransform;
 				if (GetHudMode())
 				{
-					Device.mProject.build_projection(deg2rad(psHUD_FOV * Device.fFOV),
+					RDEVICE.mProject.build_projection(deg2rad(psHUD_FOV * Device.fFOV),
 						Device.fASPECT,
-						VIEWPORT_NEAR,
+						VIEWPORT_NEAR_HUD,
 						g_pGamePersistent->Environment().CurrentEnv->far_plane);
 
 					Device.mFullTransform.mul(Device.mProject, Device.mView);
