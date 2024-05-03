75113fad19d9831b53310a6b878d8bdbb8e3c24b
AlistairFox <foxforstop@gmail.com>
2024-05-04 02:55:10 +1000

    * Minor Changes

--- src/xrGame/game_sv_freemp_dynamic_weather.cpp
+++ src/xrGame/game_sv_freemp_dynamic_weather.cpp
@@ -48,8 +48,10 @@ void game_sv_freemp::DynamicWeatherUpdate()
 				float random = 0.0f;
 				need_change_weather = true;
 				random += Random.randF(0.f, 1.f);
-				shared_str def;
+				shared_str def = "";
 				shared_str curr_weather = g_pGamePersistent->Environment().CurrentWeatherName;
+				shared_str News_Text = "";
+				shared_str Icon = "";
 
 				while (!changeweather)
 				{
@@ -77,13 +79,8 @@ void game_sv_freemp::DynamicWeatherUpdate()
 
 					if (random <= 0.25f && (xr_strcmp(curr_weather, "[main_cycle_multy]") != 0) && !changeweather)
 					{
-						NET_Packet P;
-						GenerateGameMessage(P);
-						P.w_u32(GAME_EVENT_WEATHER_UPDATE);
-						P.w_stringZ("Weather News");
-						P.w_stringZ("По прогнозу на завтра: Переменная облачность");
-						P.w_stringZ("ui_inGame2_Radiopomehi");
-						server().SendBroadcast(BroadcastCID, P, net_flags(true, true));
+						News_Text = "По прогнозу на завтра: Переменная облачность";
+						Icon = "ui_inGame2_Radiopomehi";
 						def = "[main_cycle_multy]";
 						random = 0.0f;
 						changeweather = true;
@@ -91,13 +88,8 @@ void game_sv_freemp::DynamicWeatherUpdate()
 
 					if (random <= 0.50f && (xr_strcmp(curr_weather, "[main_cycle_foggy_day]") != 0) && !changeweather)
 					{
-						NET_Packet P;
-						GenerateGameMessage(P);
-						P.w_u32(GAME_EVENT_WEATHER_UPDATE);
-						P.w_stringZ("Weather News");
-						P.w_stringZ("По прогнозу завтра сильная туманность!");
-						P.w_stringZ("ui_inGame2_Radiopomehi");
-						server().SendBroadcast(BroadcastCID, P, net_flags(true, true));
+						News_Text = "По прогнозу завтра сильная туманность!";
+						Icon = "ui_inGame2_Radiopomehi";
 						def = "[main_cycle_foggy_day]";
 						random = 0.0f;
 						changeweather = true;
@@ -105,13 +97,8 @@ void game_sv_freemp::DynamicWeatherUpdate()
 
 					if (random <= 0.75f && (xr_strcmp(curr_weather, "[main_cycle_sunny_day]") != 0) && !changeweather)
 					{
-						NET_Packet P;
-						GenerateGameMessage(P);
-						P.w_u32(GAME_EVENT_WEATHER_UPDATE);
-						P.w_stringZ("Weather News");
-						P.w_stringZ("Завтрашний день обещает быть солнечным!");
-						P.w_stringZ("ui_inGame2_Radiopomehi");
-						server().SendBroadcast(BroadcastCID, P, net_flags(true, true));
+						News_Text = "Завтрашний день обещает быть солнечным!";
+						Icon = "ui_inGame2_Radiopomehi";
 						random = 0.0f;
 						def = "[main_cycle_sunny_day]";
 						changeweather = true;
@@ -119,13 +106,8 @@ void game_sv_freemp::DynamicWeatherUpdate()
 
 					if (random > 0.75f && (xr_strcmp(curr_weather, "[main_cycle_thunder_year]") != 0) && !changeweather)
 					{
-						NET_Packet P;
-						GenerateGameMessage(P);
-						P.w_u32(GAME_EVENT_WEATHER_UPDATE);
-						P.w_stringZ("Weather News");
-						P.w_stringZ("Внимание! Завтра будет сильная буря!");
-						P.w_stringZ("ui_inGame2_Radiopomehi");
-						server().SendBroadcast(BroadcastCID, P, net_flags(true, true));
+						News_Text = "Внимание! Завтра будет сильная буря!";
+						Icon = "ui_inGame2_Radiopomehi";
 						random = 0.0f;
 						def = "[main_cycle_thunder_year]";
 						changeweather = true;
@@ -135,6 +117,14 @@ void game_sv_freemp::DynamicWeatherUpdate()
 				if (changeweather)
 				{
 					Msg("- Calculate end");
+
+					NET_Packet P;
+					GenerateGameMessage(P);
+					P.w_u32(GAME_EVENT_WEATHER_UPDATE);
+					P.w_stringZ("Weather News");
+					P.w_stringZ(News_Text);
+					P.w_stringZ(Icon);
+					server().SendBroadcast(BroadcastCID, P, net_flags(true, true));
 					g_pGamePersistent->Environment().SetWeather(def, true);
 					changeweather = false;
 					weather_will_change = true;
