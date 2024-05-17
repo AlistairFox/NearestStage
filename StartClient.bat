7c95bb3cd9e2bc08e9a519acb61e23f6b033fcc9
AlistairFox <foxforstop@gmail.com>
2024-05-17 11:05:17 +1000

    Cleaning

--- StartClient.bat
+++
@@ -1,2 +0,0 @@
-echo y|rmdir /s _client_\users\shaders_cache\
-start bins\xrEngine.exe -fsltx fsgameClient.ltx
--- StartServer_omp.bat
+++
@@ -1,2 +0,0 @@
-cd bins
-start ..\dedicated\xrEngine.exe -fsltx ..\fsgameDedicated.ltx -dedicated -i -start server(omp_test/rp/portsv=5400) client(localhost)
