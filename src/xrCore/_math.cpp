eb7d95aa8ae93e53a0481e6afb692039d199fea8
AlistairFox <foxforstop@gmail.com>
2024-06-11 20:52:22 +1000

    minor changes

--- src/xrCore/_math.cpp
+++ src/xrCore/_math.cpp
@@ -262,7 +262,7 @@ void _initialize_cpu(void)
 	if (CPU::ID.hasFeature(CPUFeature::AVX))
 		xr_strcat(features, ", AVX");
 #ifdef __AVX__
-	else Debug.do_exit(NULL, "X-Ray x64 using AVX anyway!");
+	else Debug.fatal(__FILE__, __LINE__, __FUNCTION__, "X-Ray x64 using AVX anyway!");
 #endif
 
 	if (CPU::ID.hasFeature(CPUFeature::AVX2))
